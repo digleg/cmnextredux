29588f7cc94eedfd5d5d8c41bb1d08e4763e27c3
vito huang <digleg_h@hotmail.com>
2020-12-18 13:17:59 +0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+// tsrfc
+import React, { ReactElement } from "react";
+
+interface Props {}
+
+export default function index({}: Props): ReactElement {
+  return (
+    <div>
+      <h1>codeMobile</h1>
+    </div>
+  );
+}
